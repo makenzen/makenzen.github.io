45ad1fe11a6ff2f12a53d277f610427b67431197
Joanna Morawska <makenzen@gmail.com>
2018-02-07 23:41:58 +0100

    pobierz dane

--- zadania-domowe/zadanie-ajax/js/main.js
+++ zadania-domowe/zadanie-ajax/js/main.js
@@ -9,7 +9,7 @@ function getData() {
     $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
 
         $('#zasysacz-danych').click(function () {
-            $("button").after($('<div id="dane-programisty"></div>'));
+            $("button").after($("<div id="dane-programisty"></div>"));
             $("#dane-programisty").append($("p").text("Imię: " + data.imie));
             $("#dane-programisty").append($("p").text("Nazwisko: " + data.nazwisko));
             $("#dane-programisty").append($("p").text("Zawód: " + data.zawod));
