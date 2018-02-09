6332bb940615ddde3ebd4a1cb92803268c6196a8
Joanna Morawska <makenzen@gmail.com>
2018-02-09 02:27:51 +0100

    nadal nie działa, lipa i chała

--- zadania-domowe/zadanie-ajax/js/main.js
+++ zadania-domowe/zadanie-ajax/js/main.js
@@ -5,21 +5,19 @@ $(document).ready(function () {
 });
 
 function getData() {
-    
+
     $('#zasysacz-danych').click(function () {
+
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+
             $("button").after("<div id="dane-programisty"></div>");
             $("#dane-programisty").append($("p").text("Imię: " + data.imie));
             $("#dane-programisty").append($("p").text("Nazwisko: " + data.nazwisko));
             $("#dane-programisty").append($("p").text("Zawód: " + data.zawod));
             $("#dane-programisty").append($("p").text("Firma: " + data.firma));
-            
+
         });
-
-    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-
-        
-
     });
-}
 
-//NO I CZEMU NIE DZIAŁASZ, SZMATO?!
+
+}
