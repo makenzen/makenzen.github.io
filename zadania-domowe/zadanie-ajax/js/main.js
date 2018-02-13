93763d5dc81a995f719cc820ac2cdb5c5572fe02
Joanna Morawska <makenzen@gmail.com>
2018-02-13 22:11:29 +0100

    no nareszcie działa

--- zadania-domowe/zadanie-ajax/js/main.js
+++ zadania-domowe/zadanie-ajax/js/main.js
@@ -8,13 +8,13 @@ function getData() {
 
     $('#zasysacz-danych').click(function () {
 
-        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (dane) {
 
             $("button").after($("<div id=\"dane-programisty\"></div>"));
-            $("#dane-programisty").append($("p").text("Imię: " + data.imie));
-            $("#dane-programisty").append($("p").text("Nazwisko: " + data.nazwisko));
-            $("#dane-programisty").append($("p").text("Zawód: " + data.zawod));
-            $("#dane-programisty").append($("p").text("Firma: " + data.firma));
+            $("#dane-programisty").append($("<p>").text("Imię: " + dane.imie));
+            $("#dane-programisty").append($("<p>").text("Nazwisko: " + dane.nazwisko));
+            $("#dane-programisty").append($("<p>").text("Zawód: " + dane.zawod));
+            $("#dane-programisty").append($("<p>").text("Firma: " + dane.firma));
 
         });
     });
