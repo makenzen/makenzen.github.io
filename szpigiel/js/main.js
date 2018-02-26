f6a5734d75904e3c786360a09acc81495b95a37d
Joanna Morawska <makenzen@gmail.com>
2018-02-26 13:47:18 +0100

    gotowe (wersja na partyzanta)

--- szpigiel/js/main.js
+++ szpigiel/js/main.js
@@ -1,11 +1,201 @@
 $(document).ready(function(){
-    $(".strona").click(function(){
-        $(".strona").animate().css({
+    $('.slask-1').click(function(){
+        $('.slask-1').animate().css({
            'background-color': 'blue'
         });
     });
-    $(".strona").dblclick(function(){
-        $(".strona").animate().css({
+    $('.slask-1').dblclick(function(){
+        $('.slask-1').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.slask-2').click(function(){
+        $('.slask-2').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.slask-2').dblclick(function(){
+        $('.slask-2').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.slask-3').click(function(){
+        $('.slask-3').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.slask-3').dblclick(function(){
+        $('.slask-3').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.slask-4').click(function(){
+        $('.slask-4').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.slask-4').dblclick(function(){
+        $('.slask-4').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.dlnslask-1').click(function(){
+        $('.dlnslask-1').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.dlnslask-1').dblclick(function(){
+        $('.dlnslask-1').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.dlnslask-2').click(function(){
+        $('.dlnslask-2').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.dlnslask-2').dblclick(function(){
+        $('.dlnslask-2').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.dlnslask-3').click(function(){
+        $('.dlnslask-3').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.dlnslask-3').dblclick(function(){
+        $('.dlnslask-3').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.dlnslask-4').click(function(){
+        $('.dlnslask-4').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.dlnslask-4').dblclick(function(){
+        $('.dlnslask-4').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.pom-1').click(function(){
+        $('.pom-1').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.pom-1').dblclick(function(){
+        $('.pom-1').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.pom-2').click(function(){
+        $('.pom-2').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.pom-2').dblclick(function(){
+        $('.pom-2').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.pom-3').click(function(){
+        $('.pom-3').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.pom-3').dblclick(function(){
+        $('.pom-3').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.pom-4').click(function(){
+        $('.pom-4').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.pom-4').dblclick(function(){
+        $('.pom-4').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.lodz-1').click(function(){
+        $('.lodz-1').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.lodz-1').dblclick(function(){
+        $('.lodz-1').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.lodz-2').click(function(){
+        $('.lodz-2').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.lodz-2').dblclick(function(){
+        $('.lodz-2').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.lodz-3').click(function(){
+        $('.lodz-3').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.lodz-3').dblclick(function(){
+        $('.lodz-3').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.lodz-4').click(function(){
+        $('.lodz-4').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.lodz-4').dblclick(function(){
+        $('.lodz-4').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.maz-1').click(function(){
+        $('.maz-1').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.maz-1').dblclick(function(){
+        $('.maz-1').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.maz-2').click(function(){
+        $('.maz-2').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.maz-2').dblclick(function(){
+        $('.maz-2').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.maz-3').click(function(){
+        $('.maz-3').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.maz-3').dblclick(function(){
+        $('.maz-3').animate().css({
+           'background-color': 'yellow'
+        });
+    });
+    $('.maz-4').click(function(){
+        $('.maz-4').animate().css({
+           'background-color': 'blue'
+        });
+    });
+    $('.maz-4').dblclick(function(){
+        $('.maz-4').animate().css({
            'background-color': 'yellow'
         });
     });
